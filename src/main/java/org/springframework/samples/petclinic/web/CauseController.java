31ca465183346d3e3f8de4cd37e559f8ff312e45
fraperbar <fraperbar@alum.us.es>
2020-04-05 14:10:07 +0200

    fixed some bugs

--- src/main/java/org/springframework/samples/petclinic/web/CauseController.java
+++ src/main/java/org/springframework/samples/petclinic/web/CauseController.java
@@ -3,12 +3,18 @@ package org.springframework.samples.petclinic.web;
 import java.util.Collection;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.model.Cause;
 import org.springframework.samples.petclinic.model.Donation;
 import org.springframework.samples.petclinic.service.ClinicService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
